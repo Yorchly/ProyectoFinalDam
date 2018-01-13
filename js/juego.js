2c78caa80a0606ac26ebc6e11059fc87aa2ac4fe
Yorchly <jorge.gutierrezvila0@gmail.com>
2018-01-13 18:50:58 +0100

    Añadida hit area a las respuestas del juego

--- js/juego.js
+++ js/juego.js
@@ -46,6 +46,8 @@ function cargarImagen()
 
 function cargarPregunta()
 {
+	//stage = new createjs.Stage("escenario");
+	stage.enableMouseOver(10);
 	var pregunta = new createjs.Text("¿A qué cómic pertenece esta imagen?", "34px Arial", "#1726C6");
 	var resp1 = new createjs.Text("A) Batman: Dark Knight Returns", "34px Arial", "#497DF0");
 	var resp2 = new createjs.Text("B) Civil War", "34px Arial", "#497DF0");
@@ -57,16 +59,56 @@ function cargarPregunta()
 	pregunta.y=300;
 	resp1.x = 100;
 	resp1.y = 350;
+	resp1.alpha = 0.5;
 	resp2.x = 100;
 	resp2.y = 400;
+	resp2.alpha = 0.5;
 	resp3.x = 100;
 	resp3.y = 450;
+	resp3.alpha = 0.5;
 	resp4.x = 600;
 	resp4.y = 350;
+	resp4.alpha = 0.5;
 	resp5.x = 600;
 	resp5.y = 400;
+	resp5.alpha = 0.5;
 	resp6.x = 600;
 	resp6.y = 450;
+	resp6.alpha = 0.5;
+
+	//Añade hit area a las respuestas.
+	var hit = new createjs.Shape();
+	var hit1 = new createjs.Shape();
+	var hit2 = new createjs.Shape();
+	var hit3 = new createjs.Shape();
+	var hit4 = new createjs.Shape();
+	var hit5 = new createjs.Shape();
+	hit.graphics.beginFill("#000").drawRect(0, 0, resp1.getMeasuredWidth(), resp1.getMeasuredHeight());
+	resp1.hitArea = hit;
+	hit1.graphics.beginFill("#000").drawRect(0, 0, resp2.getMeasuredWidth(), resp2.getMeasuredHeight());
+	resp2.hitArea = hit1;
+	hit2.graphics.beginFill("#000").drawRect(0, 0, resp3.getMeasuredWidth(), resp3.getMeasuredHeight());
+	resp3.hitArea = hit2;
+	hit3.graphics.beginFill("#000").drawRect(0, 0, resp4.getMeasuredWidth(), resp4.getMeasuredHeight());
+	resp4.hitArea = hit3;
+	hit4.graphics.beginFill("#000").drawRect(0, 0, resp5.getMeasuredWidth(), resp5.getMeasuredHeight());
+	resp5.hitArea = hit4;
+	hit5.graphics.beginFill("#000").drawRect(0, 0, resp6.getMeasuredWidth(), resp6.getMeasuredHeight());
+	resp6.hitArea = hit5;
+   
+	resp1.on("mouseover", handleInteraction);
+	resp1.on("mouseout", handleInteraction);
+	resp2.on("mouseover", handleInteraction);
+	resp2.on("mouseout", handleInteraction);
+	resp3.on("mouseover", handleInteraction);
+	resp3.on("mouseout", handleInteraction);
+	resp4.on("mouseover", handleInteraction);
+	resp4.on("mouseout", handleInteraction);
+	resp5.on("mouseover", handleInteraction);
+	resp5.on("mouseout", handleInteraction);
+	resp6.on("mouseover", handleInteraction);
+	resp6.on("mouseout", handleInteraction);
+
 	switch(numImagen)
 	{
 		case 1:
@@ -120,6 +162,11 @@ function cargarPregunta()
 	}
     stage.addChild(pregunta, resp1, resp2, resp3, resp4, resp5, resp6);
     stage.update();
+}
+
+
+function handleInteraction(event) {
+    event.target.alpha = (event.type == "mouseover") ? 1 : 0.5; 
 }
 
 function acierto(event){
